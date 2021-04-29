1a41027183ab171c2366b227b58ed99d7da85f77
Danil <orlov@stranzit.ru>
2021-04-29 17:55:56 +0800

    добавил распознование svg, разделил css на модули

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = (env, argv) => {
                     ],
                 },
                 {
-                    test: /\.(png|jpe?g|gif)$/i,
+                    test: /\.(png|jpe?g|gif|svg)$/i,
                     loader: 'file-loader',
                     options: {
                       name: './images/[name].[ext]',
