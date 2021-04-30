d8aef86de0783fdc13a26beb88066a514158d348
Danil <orlov@stranzit.ru>
2021-04-30 11:33:53 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './style/index.css'
 
+const btnCloseMenu = document.querySelector('#btnCloseMenu');
+const btnOpenMenu = document.querySelector('#btnOpenMenu');
+
+const sideMenuMobile = document.querySelector('.side-menu_mobile');
+const sideMenu = document.querySelector('.side-menu');
+
+btnOpenMenu.addEventListener('click', () => {
+    sideMenuMobile.style = 'display: none';
+    sideMenu.style = 'display: block';
+})
+
+btnCloseMenu.addEventListener('click', () => {
+    sideMenu.style = 'display: none';
+    sideMenuMobile.style = 'display: block';
+})
+
 
 console.log('Ghbdtm!!!')
