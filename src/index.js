bb8cb5b0b67a98f262c061a56700156e349858ff
Danil <ordanan38@gmail.com>
2021-05-02 14:14:05 +0800

    v-1

--- src/index.js
+++ src/index.js
@@ -6,21 +6,31 @@ const btnOpenMenu = document.querySelector('#btnOpenMenu');
 const sideMenuMobile = document.querySelector('.side-menu_mobile');
 const sideMenu = document.querySelector('.side-menu');
 
+const containerInput = document.querySelector('.search__container-input');
+const inputImg = document.querySelector('.search__input-img');
+
+// открываем меню
 btnOpenMenu.addEventListener('click', () => {
-    sideMenuMobile.style = 'display: none';
-    sideMenu.style = 'display: block';
+  sideMenuMobile.style = 'display: none';
+  sideMenu.style = 'display: block';
 })
 
+// закрываем меню
 btnCloseMenu.addEventListener('click', () => {
-    sideMenu.style = 'display: none';
-    sideMenuMobile.style = 'display: block';
+  sideMenu.style = 'display: none';
+  sideMenuMobile.style = 'display: block';
 })
 
+// обработка поиска при скроле. Если хотим уйти position: sticy, необходимо добавить position: fixed
 window.addEventListener("scroll", () =>{
-// в зависимости от pageYOffset присваеваем клас для инпута
-    console.log(pageYOffset + 'px')
-
-    });
-
-
-console.log('Ghbdtm!!!')
+  if (pageYOffset > 50) {
+    containerInput.classList.add('_top-input');
+    if (window.screen.availWidth < 440) {
+      inputImg.classList.add('_hidden');
+    }
+    console.log(window.screen.availWidth)
+  } else {
+    containerInput.classList.remove('_top-input');
+    inputImg.classList.remove('_hidden');
+  }
+});
